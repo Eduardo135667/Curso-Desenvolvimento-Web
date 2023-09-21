e2344845426d0211459b82362128c7c73b4fd899
Eduardo135667 <el2210@gmail.com>
2023-09-20 21:53:13 -0300

    layouts

---
+++ JavaScript/meu_script.js
@@ -0,0 +1,9 @@
+alert('Olá seja bem vindo ao curso!!!')
+document.getElementById('nome').value = 'Oi'
+
+// Adicionando comentario em uma linha.
+/*
+   comentario de multiplas linhas
+   selecionar um elemento no DOM
+   atulizar o valor desse elemento com uma string
+   */
